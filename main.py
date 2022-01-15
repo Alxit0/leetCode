35f8b113d56c71548bf20923afd8ae989adc2ed3
Alex_pc <alexandreregalado3961@gmail.com>
2022-01-15 19:29:26 +0000

    exercicio

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Hello world!')
+"""
+0 -> ''
+1 -> '()'
+"""
+
+
+def generate_pare(lvl: int, top=None, rep=0):
+    if top is None:
+        top = {''}
+
+    if lvl == 0:
+        # print(rep)
+        return top
+
+    new_set = set()
+    for i in top:
+        for j in [f'(){i}', f'({i})', f'{i}()']:
+            if j not in new_set:
+                new_set.add(j)
+            rep += 1
+
+    return generate_pare(lvl-1, new_set, rep)
+
+
+if __name__ == '__main__':
+    a = 3
+
+    print(generate_pare2(a), generate_pare(a))
+
---
+++ problems/Medium/1824. Minimum Sideway Jumps.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def minSideJumps(self, obstacles: List[int]) -> int:
+        jumps = 0
+        for i in obstacles[1:-1]:
+            pass
+        return 0
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.minSideJumps([0, 1, 2, 3, 0])
